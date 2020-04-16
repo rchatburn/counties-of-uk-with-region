8e598d9d1662904f3156812ddfcc20a8c100cfa0
rchatburn <rchatburn@gmail.com>
2020-04-16 12:59:09 +0100

    Create uk-counties-with-region.php

---
+++ uk-counties-with-region.php
@@ -0,0 +1,456 @@
+<?php 
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | UK Counties
+    |--------------------------------------------------------------------------
+    */
+    'counties' => [
+        'England' =>
+        [
+            [
+                'county' => 'Bedfordshire',
+                'region' => 'East',
+            ],
+            [
+                'county' => 'Buckinghamshire',
+                'region' => 'South East',
+            ],
+            [
+                'county' => 'Cambridgeshire',
+                'region' => 'East',
+            ],
+            [
+                'county' => 'Cheshire',
+                'region' => 'North West',
+            ],
+            [
+                'county' => 'Redcar & Cleveland',
+                'region' => 'North East',
+            ],
+            [
+                'county' => 'Cornwall',
+                'region' => 'South West',
+            ],
+            [
+                'county' => 'Cumbria',
+                'region' => 'North West',
+            ],
+            [
+                'county' => 'Derbyshire',
+                'region' => 'East Midlands',
+            ],
+            [
+                'county' => 'Devon',
+                'region' => 'South West',
+            ],
+            [
+                'county' => 'Dorset',
+                'region' => 'South West',
+            ],
+
+            [
+                'county' => 'Durham',
+                'region' => 'North East',
+            ],
+
+            [
+                'county' => 'East Sussex',
+                'region' => 'South East',
+            ],
+
+            [
+                'county' => 'Essex',
+                'region' => 'East',
+            ],
+
+            [
+                'county' => 'Gloucestershire',
+                'region' => 'South West',
+            ],
+
+            [
+                'county' => 'Greater London',
+                'region' => 'London',
+            ],
+
+            [
+                'county' => 'Greater Manchester',
+                'region' => 'North West',
+            ],
+
+            [
+                'county' => 'Hampshire',
+                'region' => 'South East',
+            ],
+
+            [
+                'county' => 'Hertfordshire',
+                'region' => 'East',
+            ],
+
+            [
+                'county' => 'Kent',
+                'region' => 'South East',
+            ],
+
+            [
+                'county' => 'Lancashire',
+                'region' => 'North West',
+            ],
+
+            [
+                'county' => 'Leicestershire',
+                'region' => 'East Midlands',
+            ],
+
+            [
+                'county' => 'Lincolnshire',
+                'region' => 'East Midlands',
+            ],
+
+            [
+                'county' => 'Merseyside',
+                'region' => 'North West',
+            ],
+
+            [
+                'county' => 'Norfolk',
+                'region' => 'East',
+            ],
+
+            [
+                'county' => 'North Yorkshire',
+                'region' => 'Yorkshire and the Humber',
+            ],
+
+            [
+                'county' => 'Northamptonshire',
+                'region' => 'East Midlands',
+            ],
+
+            [
+                'county' => 'Northumberland',
+                'region' => 'North East',
+            ],
+
+            [
+                'county' => 'Nottinghamshire',
+                'region' => 'East Midlands',
+            ],
+
+            [
+                'county' => 'Oxfordshire',
+                'region' => 'South East',
+            ],
+
+            [
+                'county' => 'Shropshire',
+                'region' => 'West Midlands',
+            ],
+
+            [
+                'county' => 'Somerset',
+                'region' => 'South West',
+            ],
+
+            [
+                'county' => 'South Yorkshire',
+                'region' => 'Yorkshire and the Humber',
+            ],
+
+            [
+                'county' => 'Staffordshire',
+                'region' => 'West Midlands',
+            ],
+
+            [
+                'county' => 'Suffolk',
+                'region' => 'East',
+            ],
+
+            [
+                'county' => 'Surrey',
+                'region' => 'South East',
+            ],
+
+            [
+                'county' => 'Tyne and Wear',
+                'region' => 'North East',
+            ],
+
+            [
+                'county' => 'Warwickshire',
+                'region' => 'West Midlands',
+            ],
+
+            [
+                'county' => 'West Berkshire',
+                'region' => 'South East',
+            ],
+
+            [
+                'county' => 'West Midlands',
+                'region' => 'West Midlands',
+            ],
+
+            [
+                'county' => 'West Sussex',
+                'region' => 'South East',
+            ],
+
+            [
+                'county' => 'West Yorkshire',
+                'region' => 'Yorkshire and the Humber',
+            ],
+
+            [
+                'county' => 'Wiltshire',
+                'region' => 'South West',
+            ],
+
+            [
+                'county' => 'Worcestershire',
+                'region' => 'West Midlands',
+            ],
+        ],
+        'Wales' =>
+        [
+            [
+                'county' => 'Flintshire',
+                'region' => 'Wales',
+            ],
+            [
+                'county' => 'Glamorgan',
+                'region' => 'Wales',
+            ],
+            [
+                'county' => 'Merionethshire',
+                'region' => 'Wales',
+            ],
+            [
+                'county' => 'Monmouthshire',
+                'region' => 'Wales',
+            ],
+            [
+                'county' => 'Montgomeryshire',
+                'region' => 'Wales',
+            ],
+            [
+                'county' => 'Pembrokeshire',
+                'region' => 'Wales',
+            ],
+            [
+                'county' => 'Radnorshire',
+                'region' => 'Wales',
+            ],
+            [
+                'county' => 'Anglesey',
+                'region' => 'Wales',
+            ],
+            [
+                'county' => 'Breconshire',
+                'region' => 'Wales',
+            ],
+            [
+                'county' => 'Caernarvonshire',
+                'region' => 'Wales',
+            ],
+
+            [
+                'county' => 'Cardiganshire',
+                'region' => 'Wales',
+            ],
+
+            [
+                'county' => 'Carmarthenshire',
+                'region' => 'Wales',
+            ],
+
+            [
+                'county' => 'Denbighshire',
+                'region' => 'Wales',
+            ],
+        ],
+        'Scotland' =>
+        [
+            [
+                'county' => 'Aberdeen City',
+                'region' => 'Scotland',
+            ],
+            [
+                'county' => 'Aberdeenshire',
+                'region' => 'Scotland',
+            ],
+            [
+                'county' => 'Angus',
+                'region' => 'Scotland',
+            ],
+            [
+                'county' => 'Argyll and Bute',
+                'region' => 'Scotland',
+            ],
+            [
+                'county' => 'City of Edinburgh',
+                'region' => 'Scotland',
+            ],
+            [
+                'county' => 'Clackmannanshire',
+                'region' => 'Scotland',
+            ],
+            [
+                'county' => 'Dumfries and Galloway',
+                'region' => 'Scotland',
+            ],
+            [
+                'county' => 'Dundee City',
+                'region' => 'Scotland',
+            ],
+            [
+                'county' => 'East Ayrshire',
+                'region' => 'Scotland',
+            ],
+            [
+                'county' => 'East Dunbartonshire',
+                'region' => 'Scotland',
+            ],
+
+            [
+                'county' => 'East Lothian',
+                'region' => 'Scotland',
+            ],
+
+            [
+                'county' => 'East Renfrewshire',
+                'region' => 'Scotland',
+            ],
+
+            [
+                'county' => 'Eilean Siar',
+                'region' => 'Scotland',
+            ],
+
+            [
+                'county' => 'Falkirk',
+                'region' => 'Scotland',
+            ],
+
+            [
+                'county' => 'Fife',
+                'region' => 'Scotland',
+            ],
+
+            [
+                'county' => 'Glasgow City',
+                'region' => 'Scotland',
+            ],
+
+            [
+                'county' => 'Highland',
+                'region' => 'Scotland',
+            ],
+
+            [
+                'county' => 'Inverclyde',
+                'region' => 'Scotland',
+            ],
+
+            [
+                'county' => 'Midlothian',
+                'region' => 'Scotland',
+            ],
+
+            [
+                'county' => 'Moray',
+                'region' => 'Scotland',
+            ],
+
+            [
+                'county' => 'North Ayrshire',
+                'region' => 'Scotland',
+            ],
+
+            [
+                'county' => 'North Lanarkshire',
+                'region' => 'Scotland',
+            ],
+
+            [
+                'county' => 'Orkney Islands',
+                'region' => 'Scotland',
+            ],
+
+            [
+                'county' => 'Perth and Kinross',
+                'region' => 'Scotland',
+            ],
+
+            [
+                'county' => 'Renfrewshire',
+                'region' => 'Scotland',
+            ],
+
+            [
+                'county' => 'Scottish Borders',
+                'region' => '',
+            ],
+
+            [
+                'county' => 'Shetland Islands',
+                'region' => 'Scotland',
+            ],
+
+            [
+                'county' => 'South Ayrshire',
+                'region' => 'Scotland',
+            ],
+
+            [
+                'county' => 'South Lanarkshire',
+                'region' => 'Scotland',
+            ],
+
+            [
+                'county' => 'Stirling',
+                'region' => 'Scotland',
+            ],
+
+            [
+                'county' => 'West Dunbartonshire',
+                'region' => 'Scotland',
+            ],
+
+            [
+                'county' => 'West Lothian',
+                'region' => 'Scotland',
+            ],
+        ],
+        'Northern Ireland' =>
+        [
+            [
+                'county' => 'Antrim',
+                'region' => 'Northern Ireland',
+            ],
+            [
+                'county' => 'Armagh',
+                'region' => 'Northern Ireland',
+            ],
+            [
+                'county' => 'Down',
+                'region' => 'Northern Ireland',
+            ],
+            [
+                'county' => 'Fermanagh',
+                'region' => 'Northern Ireland',
+            ],
+            [
+                'county' => 'Derry and Londonderry',
+                'region' => 'Northern Ireland',
+            ],
+            [
+                'county' => 'Tyrone',
+                'region' => 'Northern Ireland',
+            ]
+        ]
+    ]
